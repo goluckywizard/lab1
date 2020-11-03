4764523c04b985db27f8be70bc1d11968df74e2d
goluckywizard <73885997+goluckywizard@users.noreply.github.com>
2020-11-03 21:42:44 +0700

    Update main.c

--- main.c
+++ main.c
@@ -5,7 +5,7 @@
 /*class Trit{
 
 };*/
-enum Trit{False, Unknown, True};
+enum Trit{False = 1, Unknown = 2, True = 3};
 class Tritset {
 private:
     std::vector<unsigned int> forTrits;
@@ -18,23 +18,25 @@ public: Tritset (size_t size, const std::string& str, char True = 'T', char Unkn
     unsigned int ins_vec = 0;
     for (size_t i = 0; i < str.size(); i++)
     {
-        if (new_uint == sizeof(unsigned int))
-        {
-            forTrits.push_back(ins_vec);
-            new_uint = 0;
-            ins_vec = 0;
-        }
         if (str[i] == True)
         {
             ins_vec |= 0b11;
-        }
-        if (str[i] == Unknown)
-        {
-            ins_vec |= 0b10;
-        }
+        } else
         if (str[i] == False)
         {
             ins_vec |= 0b00;
+        } else
+        {
+            ins_vec |= 0b10;
+        }
+        //std::cout << ins_vec << " ";
+        if (new_uint == sizeof(unsigned int) * 8)
+        {
+            //std::cout << "kuk" << new_uint;
+            forTrits.push_back(ins_vec);
+            new_uint = 0;
+            ins_vec = 0;
+            continue;
         }
         ins_vec <<= 2;
         new_uint += 2;
@@ -42,33 +44,45 @@ public: Tritset (size_t size, const std::string& str, char True = 'T', char Unkn
     while (new_uint != 0)
     {
         ins_vec |= 0b10;
-        ins_vec <<= 2;
-        new_uint += 2;
-        if (new_uint == sizeof(unsigned int))
+        if (new_uint == sizeof(unsigned int) * 8 - 2)
         {
             forTrits.push_back(ins_vec);
-            new_uint = 0;
-            ins_vec = 0;
+            break;
         }
+        ins_vec <<= 2;
+        new_uint += 2;
+
     }
 }
     size_t capacity()
     {
         return forTrits.size();
     }
-    int&operator[](const size_t number){
-
-        }
-    Tritset&operator[](const size_t number){
+    int operator[](const size_t number){
+        unsigned int comp = forTrits[forTrits.size() - number * 2 / 8 / sizeof(unsigned int) - 1];
+        //std::cout << comp << " ";
+        comp >>= (sizeof(unsigned int) * 8 - ((number * 2) % (sizeof(unsigned int) * 8))) - 2;
+        //std::cout << comp << " ";
+        comp &= 0b11;
+        //std::cout << comp << " ";
+        if (comp == 3)
+            return True;
+        if (comp == 0)
+            return False;
+        return Unknown;
+    }
+    /*Tritset&operator[](const size_t number){
             return
-        };
+        };*/
 };
 
 //using namespace std;
 
 int main() {
-    Tritset set(500);
-    set[0] = False;
-    std::cout << set.capacity();
+    Tritset set(3, "TFT");
+    //std::cout << std::endl;
+    for (int i = 0; i < 16; ++i)
+        std:: cout << set[i]  << " ";
+    //std::cout << set.capacity() << sizeof(unsigned int);
     return 0;
 }
